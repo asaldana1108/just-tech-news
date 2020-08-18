e00a901a954922b9d4bdc13e50c1be1009e9fe0d
asaldana1108 <asaldana1108@gmail.com>
2020-08-18 11:59:18 -0500

    populate template with sequelize data

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+//express static middleware function for front end specific files like stylesheet, images, and JS files - public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// setting up handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
